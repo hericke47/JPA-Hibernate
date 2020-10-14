84c1f2d3eb44ef90d5c745aa63f2d188b07cb7c1
Herick Exterkoetter <herickherick47@gmail.com>
2020-10-14 15:16:43 -0300

    :fire: feat: insertion objects on database

--- jpaMavem/src/main/java/aplicacao/Programa.java
+++ jpaMavem/src/main/java/aplicacao/Programa.java
@@ -1,18 +1,34 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(2, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(3, "Ana Maria", "ana@gmail.com");
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
-
+		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa"); // instanciando com as confs do arquivo persistence.xml
+		EntityManager em = emf.createEntityManager();
+		
+		// codigo para salvar as pessoas criadas no banco
+	
+		em.getTransaction().begin();
+		
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		
+		em.getTransaction().commit();
+		
+		System.out.println("Pronto! ");
+	
 	}
 
 }
